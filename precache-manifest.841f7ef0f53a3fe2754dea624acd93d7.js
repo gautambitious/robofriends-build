16726e32cbc56500c33676bc0dcd70c08f6d3b27
gautambitious <mvgt.gautam@gmail.com>
2019-12-08 02:03:53 +0530

    Initial Commit

---
+++ precache-manifest.841f7ef0f53a3fe2754dea624acd93d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70adc5b7d91d216e09df031d5315272b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "./static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "44a3a6ce413388d3b1e3",
+    "url": "./static/css/main.867d0bc3.chunk.css"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "./static/js/2.38e71f33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.38e71f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44a3a6ce413388d3b1e3",
+    "url": "./static/js/main.5605fe7b.chunk.js"
+  },
+  {
+    "revision": "fd0a5490a148fc9b9455",
+    "url": "./static/js/runtime-main.74af9048.js"
+  }
+]);
